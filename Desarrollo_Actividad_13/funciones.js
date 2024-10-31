77a5ac17a905a0c6fe741d7de76116c1cc987ec0
Ivan Madrid <tecno85@gmail.com>
2024-10-31 15:14:42 -0500

    Actualizando

--- Desarrollo_Actividad_13/funciones.js
+++ Desarrollo_Actividad_13/funciones.js
@@ -1,31 +1,29 @@
-
-// Creación de Funciones Matemáticas 
+// Creación de Funciones Matemáticas
 // Función Sumar
-function sumar(a ,b) {
-    return a + b;
-} 
+function sumar(a, b) {
+  return a + b;
+}
 console.log(sumar());
 
 // Función Restar
 function resta(a, b) {
-    return a - b;
+  return a - b;
 }
 console.log(resta(10, 5));
 
 // Función Multiplicación
 function multiplicar(a, b) {
-    return a * b;
+  return a * b;
 }
 console.log(multiplicar(4, 4));
 
 // Función Dividir
 function dividir(a, b) {
-    if(b === 0) {
-        return "¡ERROR! No se puede dividir por cero"
-    }
-    return a / b;
+  if (b === 0) {
+    return "¡ERROR! No se puede dividir por cero";
+  }
+  return a / b;
 }
 console.log(dividir(50, 2));
 
 // Implementar un Calculadora Simple
-
