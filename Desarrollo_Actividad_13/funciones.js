a15364f4e02d9b617e3c34a1bbb3c0231baa02ce
Ivan Madrid <ivan.madrid.ttc.427@unilibre.edu.co>
2024-10-31 09:39:10 -0500

    Subiendo Actividad 13

---
+++ Desarrollo_Actividad_13/funciones.js
@@ -0,0 +1,31 @@
+
+// Creación de Funciones Matemáticas 
+// Función Sumar
+function sumar(a ,b) {
+    return a + b;
+} 
+console.log(sumar());
+
+// Función Restar
+function resta(a, b) {
+    return a - b;
+}
+console.log(resta(10, 5));
+
+// Función Multiplicación
+function multiplicar(a, b) {
+    return a * b;
+}
+console.log(multiplicar(4, 4));
+
+// Función Dividir
+function dividir(a, b) {
+    if(b === 0) {
+        return "¡ERROR! No se puede dividir por cero"
+    }
+    return a / b;
+}
+console.log(dividir(50, 2));
+
+// Implementar un Calculadora Simple
+
